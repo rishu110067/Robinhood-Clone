9eef2872347348e5738b117570c83c193a6ec179
Rishu Kumar <rishu110067@gmail.com>
2022-12-31 04:39:16 +0530

    StatsRow css done

--- src/StatsRow.js
+++ src/StatsRow.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './StatsRow.css'
+import StockSVG from './stock.svg';
 
 function StatsRow(props) {
     const percentage = ((props.price - props.openPrice)/props.openPrice) * 100;
@@ -8,10 +10,12 @@ function StatsRow(props) {
                 <h1>{props.name}</h1>
                 <p>{props.volume && (props.volume + "shares")}</p>
             </div>
-
+            <div className="row_chart">
+                <img src={StockSVG} height={16} />
+            </div>
             <div className="row__numbers">
-                <p className="row__price">{props.price}</p>
-                <p className="row__percentage"> +{Number(percentage).toFixed(2)}%</p>
+                <p className="row_price">{props.price}</p>
+                <p className="row_percentage"> +{Number(percentage).toFixed(2)}%</p>
             </div>
         </div>
     )
