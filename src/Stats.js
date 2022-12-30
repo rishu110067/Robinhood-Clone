ca14b843e7a7e6c21aec62c3c144101f6af0615f
Rishu Kumar <rishu110067@gmail.com>
2022-12-31 02:48:40 +0530

    stats section updated

--- src/Stats.js
+++ src/Stats.js
@@ -4,7 +4,24 @@ import './Stats.css'
 function Stats() {
   return (
     <div className="stats">
-        Stats
+      <div className="stats_container">
+        <div className="stats_header">
+          <p>Stocks</p>
+        </div>
+        <div className="stats_content">
+          <div className="stats_rows">
+            {/* for our current stocks */}
+          </div>
+        </div>
+        <div className="stats_header">
+          <p>Lists</p>
+        </div>
+        <div className="stats_content">
+          <div className="stats_rows">
+            {/* stocks we can buy */}
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
