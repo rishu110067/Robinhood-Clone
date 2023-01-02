fd7e75fe79406e302fc13d8891e54542079ff65b
Rishu Kumar <rishu110067@gmail.com>
2023-01-02 21:05:18 +0530

    completed till popular lists

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Newsfeed.css'
 import LineGraph from './LineGraph'
+import TimeLine from './TimeLine'
+import Chip from './Chip'
+
 
 function Newsfeed() {
+    const [popularTopics, setTopics] = useState([
+        "Technology",
+        "Top Movies",
+        "Upcoming Earnings",
+        "Crypto",
+        "Cannabis",
+        "Healthcare Supplies",
+        "Index ETFs",
+        "Technology",
+        "China",
+        "Pharma",
+    ]);
+
   return (
     <div className="newsfeed">
         <div className="newsfeed_container">
@@ -13,6 +29,34 @@ function Newsfeed() {
                 </div>
                 <div className="newsfeed_chart">
                     <LineGraph />
+                    <TimeLine />
+                </div>
+            </div>
+
+            <div className="newsfeed_buying_section">
+                <h2> Buying Power</h2>
+                <h2> $4.11</h2>
+            </div>
+
+            <div className="newsfeed_market_section">
+                <div className="newsfeed_market_box">
+                    <p> Markets Closed</p>
+                    <h1> Happy New Year </h1>
+                </div>
+            </div>
+
+            <div className="newsfeed_popularlists_section">
+                <div className="newsfeed_popularlists_intro">
+                    <h1>Popular lists</h1>
+                    <p>Show More</p>
+                </div>
+                <div className="newsfeed_popularlists_badges">
+                    {popularTopics.map((topic) => (
+                        <Chip 
+                            label={topic}
+                            image={`https://avatars.dicebear.com/api/human/${topic}.svg`}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
